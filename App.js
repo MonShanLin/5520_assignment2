357cd9448ab34d125b8add2dc4c9b7b968ed39fb
monshanlin <monshanlin@gmail.com>
2024-10-11 14:47:15 -0700

    Create Context.js to manage global state and update the data

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { createContext, useState } from 'react';
 
-export default function App() {
+const DataContext = createContext();
+
+export default function DataProvider({ children }) {
+  const [entries, setEntries] = useState({
+    diet: [],
+    activities: [],
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DataContext.Provider value={{ entries, setEntries }}>
+      {children}
+    </DataContext.Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const useDataContext = () => React.useContext(DataContext);
