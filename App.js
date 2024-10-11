54c16ff1770fc4971e25109ba05d385165f567bc
monshanlin <monshanlin@gmail.com>
2024-10-11 15:21:46 -0700

    Add activity to the main screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import Diet from './Screens/Diet';
+import Activities from './Screens/Activities';
 import { DataProvider } from './Context';
 
 const Tab = createBottomTabNavigator();
@@ -17,12 +18,15 @@ export default function App() {
               let iconName;
               if (route.name === 'Diet') {
                 iconName = 'fast-food';
-              } 
+              } else if (route.name === 'Activities') {
+                iconName = 'fitness';
+              }
               return <Ionicons name={iconName} size={size} color={color} />;
             },
           })}
         >
           <Tab.Screen name="Diet" component={Diet} />
+          <Tab.Screen name="Activities" component={Activities} />
         </Tab.Navigator>
       </NavigationContainer>
     </DataProvider>
