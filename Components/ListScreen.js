a6937fee1771ef5c53329685e505ba88726318e8
monshanlin <monshanlin@gmail.com>
2024-10-11 22:15:38 -0700

    Refactor with ListScreen conponent

--- Components/ListScreen.js
+++ Components/ListScreen.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { View, TouchableOpacity, Button } from 'react-native';
+import ItemsList from '../Components/ItemsList';
+import { styles } from '../Helpers/styles';
+
+export default function ListScreen({ type, addScreenName, navigation }) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity style={{ marginRight: 15 }}>
+          <Button
+            onPress={() => navigation.navigate(addScreenName)} // Navigate to the appropriate screen
+            title="Add" // Text for the button
+            color="black" // Button text color
+          />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.screenContainer}>
+      <ItemsList type={type} />
+    </View>
+  );
+}
 
