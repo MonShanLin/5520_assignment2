e315e17542d877f1697c815a37bb31a0b50aec13
monshanlin <monshanlin@gmail.com>
2024-10-12 01:34:51 -0700

    Delete comment in Settings.js

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -4,20 +4,25 @@ import { useTheme } from '../Components/ThemeContext';
 import { styles } from '../Helpers/styles';
 import { useThemeStyles } from '../Components/useThemeStyles';
 
-
 export default function SettingsScreen() {
-  const { theme, toggleTheme } = useTheme();// Access the toggleTheme function from the custom hook
-  const { backgroundColor, textColor } = useThemeStyles(); 
+  const { theme, toggleTheme } = useTheme(); // Access the toggleTheme function from the custom hook
+  const { backgroundColor, textColor } = useThemeStyles();
 
   return (
-    <View style={[styles.screenContainer, { backgroundColor, justifyContent: 'center', alignItems: 'center' }]}>
+    <View
+      style={[
+        styles.screenContainer,
+        { backgroundColor, justifyContent: 'center', alignItems: 'center' },
+      ]}
+    >
       <TouchableOpacity
         onPress={toggleTheme}
         style={[styles.button, { backgroundColor: '#800080' }]}
       >
-        <Text style={[styles.buttonText, { color: backgroundColor }]}>Toggle Theme</Text>
+        <Text style={[styles.buttonText, { color: backgroundColor }]}>
+          Toggle Theme
+        </Text>
       </TouchableOpacity>
     </View>
   );
 }
-
