36cf991d4850388f095d08a4b56f54faa885bb17
monshanlin <monshanlin@gmail.com>
2024-10-11 18:37:58 -0700

    Add checking condition for special activity

--- Components/ItemsList.js
+++ Components/ItemsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList} from 'react-native';
 import { useDataContext } from '../Context';
 import { Ionicons } from '@expo/vector-icons';
 import { styles } from '../Helpers/styles';
@@ -7,6 +7,14 @@ import { styles } from '../Helpers/styles';
 export default function ItemsList({ type }) {
   const { entries } = useDataContext();
   const data = type === 'diet' ? entries.diet : entries.activities;
+
+  const isSpecialActivity = (item) => {
+    // Check if the activity type is "Running" or "Weight" and the duration is greater than 60 minutes
+    const duration = parseInt(item.duration); // Convert duration to integer
+    return (
+      (item.name === 'Running' || item.name === 'Weights') && duration > 60
+    );
+  };
 
   return (
     <FlatList
@@ -17,12 +25,12 @@ export default function ItemsList({ type }) {
             <Text style={styles.itemName}>{item.name}</Text>
 
           <View style={styles.itemDetailsContainer}>
-          <View style={styles.itemInfo}>
-              {new Date(item.date) < new Date() && (
-                <Ionicons name="warning" size={20} color="#FFC107" style={styles.warningIcon} />
-              )}
+            <View style={styles.itemInfo}>
+                { isSpecialActivity(item) && (
+                    <Ionicons name="warning" size={20} color="#FFC107" style={styles.warningIcon} />
+                )}
               
-              <View style={styles.box}>
+            <View style={styles.box}>
                 <Text style={styles.itemDetails}>{item.date}</Text>
               </View>
             </View>
