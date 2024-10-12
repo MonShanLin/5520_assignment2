653c6f606596c0fe0f283d8a9f05c070d80d31bc
monshanlin <monshanlin@gmail.com>
2024-10-12 01:05:07 -0700

    Handle Toggle color in ActivitiesScreen

--- Screens/Activities.js
+++ Screens/Activities.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ListScreen from '../Components/ListScreen';
+import { useThemeStyles } from '../Components/useThemeStyles'; 
 
 export default function ActivitiesScreen({ navigation }) {
-  return (
+    const { backgroundColor, textColor } = useThemeStyles(); 
+    
+    return (
     <ListScreen
       type="activities" // Pass the type as "activities"
       addScreenName="AddActivity" // Pass the screen name for the "Add" button
       navigation={navigation} // Pass the navigation prop
+      backgroundColor={backgroundColor} // Pass the background color
+      textColor={textColor} // Pass the text color
     />
   );
 }
