fc3638b9d4aa89f0ca34d429923bad849e9ab8a8
monshanlin <monshanlin@gmail.com>
2024-10-12 00:41:08 -0700

    Implement toggle color for background and Text

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -4,11 +4,15 @@ import { useTheme } from '../Components/ThemeContext';
 import { styles } from '../Helpers/styles';
 
 export default function SettingsScreen() {
-  const { toggleTheme } = useTheme();// Access the toggleTheme function from the custom hook
+  const { theme, toggleTheme } = useTheme();// Access the toggleTheme function from the custom hook
+
+  const backgroundColor = theme === 'light' ? '#ceb9ed' : '#9b64ed'; // Change background based on theme
+  const textColor = theme === 'light' ? 'black' : 'white'; // Change text color based on theme
 
   return (
-    <View style={styles.screenContainer}>
-      <Button title="Toggle Theme" onPress={toggleTheme} />
+    <View style={[styles.screenContainer, { backgroundColor }]}>
+      <Button title="Toggle Theme" onPress={toggleTheme} color={textColor} />
     </View>
   );
 }
+
