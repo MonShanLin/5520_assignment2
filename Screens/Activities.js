bfc8a2ffad489c2f26ac160030a1952a95015ac0
monshanlin <monshanlin@gmail.com>
2024-10-11 22:17:51 -0700

    Refactor Activities.js with ListScreen component

--- Screens/Activities.js
+++ Screens/Activities.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Button } from 'react-native';
-import ItemsList from '../Components/ItemsList';
-import { styles } from '../Helpers/styles';
+import ListScreen from '../Components/ListScreen';
 
 export default function ActivitiesScreen({ navigation }) {
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity style={{ marginRight: 15 }}>
-          <Button
-            onPress={() => navigation.navigate('AddActivity')}            
-            title="Add" // Text for the button
-            color="black" // Button text color
-          />
-        </TouchableOpacity>
-      ),
-    });
-  }, [navigation]);
-
   return (
-    <View style={styles.screenContainer}>
-      <ItemsList type="activities" />
-    </View>
+    <ListScreen
+      type="activities" // Pass the type as "activities"
+      addScreenName="AddActivity" // Pass the screen name for the "Add" button
+      navigation={navigation} // Pass the navigation prop
+    />
   );
 }
