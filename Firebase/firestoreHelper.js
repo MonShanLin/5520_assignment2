de205445cda066a8e037b676cc070bdcf1721409
monshanlin <monshanlin@gmail.com>
2024-10-28 04:45:07 -0700

    update firestoreHelper.js to return the docRef

--- Firebase/firestoreHelper.js
+++ Firebase/firestoreHelper.js
@@ -5,8 +5,10 @@ export async function writeToDB(data, collectionName) {
     try {
         const docRef = await addDoc(collection(database, collectionName), data);
         console.log("Document written with ID: ", docRef.id);
+        return docRef;  
     } catch (err) {
         console.log("Error writing to Firestore:", err);
+        throw err; 
     }
 }
 
