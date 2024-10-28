194b929a7b4f3a4424f314897f68bb63490abd00
monshanlin <monshanlin@gmail.com>
2024-10-28 04:41:58 -0700

    Add icon to replace add button

--- Components/ListScreen.js
+++ Components/ListScreen.js
@@ -2,20 +2,37 @@ import React from 'react';
 import { View, Pressable, Text } from 'react-native';
 import ItemsList from '../Components/ItemsList';
 import { styles } from '../Helpers/styles';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons'; 
 
 export default function ListScreen({ type, addScreenName, navigation, backgroundColor,textColor, }) {
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Pressable 
-          onPress={() => navigation.navigate(addScreenName)} 
-          style={{ marginRight: 15, padding: 10 }}
-        >
-          <Text style={{ color: 'black' }}>Add</Text>  
-        </Pressable>
+        <View style={{ flexDirection: 'row', marginRight: 15 }}>
+          <Pressable 
+            onPress={() => navigation.navigate(addScreenName)} 
+            style={{ padding: 10 }}
+          >
+            <FontAwesome5 name="plus" size={24} color="white" />
+          </Pressable>
+
+          <Pressable 
+            onPress={() => navigation.navigate(addScreenName)} 
+            style={{ marginRight: 15, padding: 10 }}
+          >
+            {type === 'diet' ? (
+              <FontAwesome5 name="utensils" size={24} color="white" />
+            ) : (
+              <FontAwesome5 name="running" size={24} color="white" />
+            )}
+          </Pressable>
+
+          
+
+        </View>
       ),
     });
-  }, [navigation]);
+  }, [navigation, type]);
 
   return (
     <View style={[styles.screenContainer, { backgroundColor }]}>
